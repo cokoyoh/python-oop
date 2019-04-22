d29497bd8b026de0cf0c030b1feb9270d41b6e80
cokoyoh <charlesokoyoh@gmail.com>
2019-04-22 15:13:48 +0300

    Add unit tests

--- oop.py
+++ oop.py
@@ -1,37 +1,39 @@
-class Employee:
+import requests
 
-    def __init__(self, first_name, last_name):
+
+class Employee:
+    raise_amount = 1.05
+
+    def __init__(self, first_name, last_name, pay):
         self.first_name = first_name
         self.last_name = last_name
+        self.pay = pay
 
     @property
     def email(self):
-        return '{}.{}@example.com'.format(self.first_name.lower(), self.last_name.lower())
+        return '{}.{}@email.com'.format(self.first_name.lower(), self.last_name.lower())
 
     @property
     def fullname(self):
         return '{} {}'.format(self.first_name, self.last_name)
 
-    @fullname.setter
-    def fullname(self, fullname):
-        first_name, last_name = fullname.split(' ')
-        self.first_name = first_name
-        self.last_name = last_name
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amount)
 
-    @fullname.deleter
-    def fullname(self):
-        print('Deleting name!')
-        self.first_name = None
-        self.last_name = None
+    def monthly_schedule(self, month):
+        response = requests.get(f'http://company.com/{self.last_name}/{month}')
+        if response.ok:
+            return response.text
+        else:
+            return 'Bad Response!'
 
 
-employee_1 = Employee('John', 'Doe')
-employee_2 = Employee('Jane', 'Doe')
+employee_1 = Employee('John', 'Doe', 50000)
+employee_2 = Employee('Jane', 'Doe', 67777)
 
-employee_1.fullname = 'Richard Roe'
 
-print(employee_1.first_name)
-print(employee_1.email)
-print(employee_1.fullname)
-del employee_1.fullname
-print(employee_1.fullname)
+# print(employee_1.first_name)
+# print(employee_1.email)
+# print(employee_1.fullname)
+# del employee_1.fullname
+# print(employee_1.fullname)
